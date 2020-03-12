ba349109ce2c6f3b8009f0175936372348b1e95c
Gabriel <49006461+bihellzin@users.noreply.github.com>
2020-03-12 16:27:09 -0300

    Update resposta.py

--- exercicios/laços-sem-listas/escada/resposta.py
+++ exercicios/laços-sem-listas/escada/resposta.py
@@ -1,10 +1,15 @@
+# Usuário digita a quantidade de degraus que quer imprimir
 n = int(input('Insira a quantidade de degraus: '))
 
+# Essas variáveis não são essenciais, mas acho que fica mais fácil de compreender a questão
 jogo_da_velha = '#'
 espacos = ' '
 
+# O contador é iniciado em 1 para que não seja necessário utilizar muita aritmética dentro do print
 cont = 1
 
+# O contador é iterado até que a quantidade de vezes que o bloco de código é executado dentro do laço 
+# seja igual ao n dado pelo usuário
 while cont <= n:
     print(espacos * (n - cont) + (jogo_da_velha * cont))
     cont += 1
