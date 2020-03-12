1f6e23f63055e28dc249b2dba576a464d8e238fc
Gabriel <49006461+bihellzin@users.noreply.github.com>
2020-03-12 13:59:36 -0300

    Update resposta.py

--- estruturas-condicionais/gatos-e-rato/resposta.py
+++ estruturas-condicionais/gatos-e-rato/resposta.py
@@ -5,20 +5,18 @@ rato = int(input('Insira a posição do rato: '))
 distancia_gatoA_rato = gato_A - rato
 
 if distancia_gatoA_rato < 0:
-  distancia_gatoA_rato = distancia_gatoA_rato * -1
-
+    distancia_gatoA_rato = distancia_gatoA_rato * -1
 
 distancia_gatoB_rato = gato_B - rato
 
 if distancia_gatoB_rato < 0:
-  distancia_gatoB_rato = distancia_gatoB_rato * -1
-
+    distancia_gatoB_rato = distancia_gatoB_rato * -1
 
 if distancia_gatoA_rato < distancia_gatoB_rato:
-  print('Gato A pega o rato')
+    print('Gato A pega o rato')
 
 elif distancia_gatoB_rato < distancia_gatoA_rato:
-  print('Gato B pega o rato')
+    print('Gato B pega o rato')
 
 else:
-  print('O rato escapou porque os gatos brigaram')
+    print('O rato escapou porque os gatos brigaram')
