71b74624b842447eac051298042831c0f8b08871
Gabriel <49006461+bihellzin@users.noreply.github.com>
2020-03-12 16:36:01 -0300

    Update resposta.py

--- exercicios/laços-sem-listas/palindromo/resposta.py
+++ exercicios/laços-sem-listas/palindromo/resposta.py
@@ -1,12 +1,19 @@
+# Recebe-se o valor do usuário
 n = int(input('Insira um número para verificar se é palíndromo: '))
 
+# A variável aux receberá o n dado pelo usuário, pois o n será utilizado para fazermos a comparação posteriormente
 aux = n
+# A variável do palíndromo é igual a zero, pois iremos incrementar o seu valor a cada iteração
 palindromo = 0
 
 while aux != 0:
+    # A cada iteração, multiplicaremos o valor atual do palíndromo por 10 e adicionaremos o resto da divisão por 10 do valor
+    # auxiliar. Após isso, aux será decrementado
     palindromo = (palindromo * 10) + (aux % 10)
     aux = aux // 10
 
+
+# Caso o valor do palíndromo seja igual ao n inicial, o número é palíndromo, caso contrário, não é.
 if palindromo == n:
     print('O número é palindromo')
 
